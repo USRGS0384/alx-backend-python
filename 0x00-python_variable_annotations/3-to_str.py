e172bc6f0f449d6aa7fa0326a1f6f32c949f0b12
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-10 14:58:39 +0000

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
-'''module task 3'''
+'''Module for task 3.
+'''
 
 
 def to_str(n: float) -> str:
-
-    '''cast a floating type number into string'''
-
-    return (str)n
+    '''Cast a floting type number in to a string.
+    '''
+    return str(n)
