99b4f06979cb8ae5ddba7c78f39286146c130258
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-10 14:34:10 +0000

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+'''Module for task 1.
+'''
 
-"""a type-annotated function concat that takes a string
-str1 and a string str2 as arguments and returns a concatenated string"""
 
-    def concat(str1: str, str2: str) -> str:
-        """concatenates two strings"""
-        return str1 + str2
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
