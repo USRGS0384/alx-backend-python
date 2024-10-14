6441cc12139a20d8799f8100d2e2469ea76cdb25
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-14 14:33:48 +0000

    0x01. Python - Async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-""" asynchronous tasks using asyncio.
-"""
-
+"""Execute multiple coroutines at the same time with async"""
+from typing import List
 import asyncio
-import random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """
-    Wait for a random amount of time up to `max_delay` seconds.
-    Returns the amount of time waited.
-    """
-    wait_time = random.random() * max_delay
-    await asyncio.sleep(wait_time)
-    return wait_timei
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with the specified max_delay
+        and returns the list of all the delays (float values)."""
+    futures = [wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
+    return delays
