55357767864e5aaae06b6bd10a34d413c160ecc1
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-14 14:35:54 +0000

    0x01. Python - Async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,8 +2,7 @@
 """Execute multiple coroutines at the same time with async"""
 from typing import List
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
-
+wait_random = __import__ ('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with the specified max_delay
