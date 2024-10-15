a7ec2273db6d9916b01a2df7bc7043434127f8ba
Philip Ajuong Deng <philipajuong@gmail.com>
2024-10-15 15:36:53 +0000

    0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
-"""
-This module provides an asynchronous generator function that yields a
-random float between 0 and 10 after a one second delay for a total of
-10 iterations.
-"""
+"""Creates a generator"""
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """
-    Asynchronous generator function that yields a random float between 0 and 10
-    after a one second delay for a total of 10 iterations.
-
-    Returns:
-        Generator: Asynchronous generator object that can be used in an
-        awaitable context.
-    """
-    for _ in range(10):
+    """Each time asynchronously waits 1 second,
+        then yield a random number between 0 and 10"""
+    for _ in range(0, 10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
